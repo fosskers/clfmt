7321b861be2596ce3da0d0821e000c93ae8a3d39
Colin Woodbury <colin@fosskers.ca>
2025-02-14 18:47:38 +0900

    [clfmt] basic file input

--- src/main.lisp
+++ src/main.lisp
@@ -7,6 +7,9 @@
 
 (in-package :clfmt)
 
+#-ecl
+(error "CLFMT can only be compiled with ECL.")
+
 (defconstant +trim+ #(#\space #\newline))
 
 (defun empty? (seq)
@@ -25,24 +28,40 @@ pair is also passed through."
             (let ((res (funcall reducer result last)))
               (funcall reducer res)))))))
 
-(defun work (path)
-  "From a PATH to a Common Lisp file, read its contents and apply reformatting."
-  (t:transduce (t:comp (t:map (lambda (line) (string-right-trim +trim+ line)))
-                       (t:drop-while #'empty?)
-                       (t:window 2)
-                       (t:filter (lambda (pair) (not (and (empty? (car pair))
-                                                          (empty? (cadr pair))))))
-                       (smart-car))
-               #'t:cons path))
+(defun for (f)
+  "Reducer: Call some function on every item to be reduced, and yield a final nil."
+  (lambda (&optional (acc nil a?) (input nil i?))
+    (declare (ignore acc))
+    (cond ((and a? i?) (funcall f input))
+          ((and a? (not i?)) nil)
+          (t nil))))
+
+(defun reformat ()
+  "Transducer: The composed logic for reformatting a stream of lines."
+  (t:comp (t:map (lambda (line) (string-right-trim +trim+ line)))
+          (t:drop-while #'empty?)
+          (t:window 2)
+          (t:filter (lambda (pair) (not (and (empty? (car pair))
+                                             (empty? (cadr pair))))))
+          (smart-car)))
+
+(defun clfmt/reformat (reducer source)
+  "From some SOURCE that yields lines, read those lines and apply simple reformatting."
+  (t:transduce (reformat) reducer source))
 
 #++
-(work #p"src/main.lisp")
+(clfmt/reformat #'t:cons #p"src/main.lisp")
 
-(defun main ())
+#++
+(defconstant +args+
+  '(("")))
 
-
-
-
+(defun main ()
+  (cond ((= 2 (length ext:*command-args*))
+         (clfmt/reformat (for (lambda (item) (format t "~a~%" item)))
+                         (pathname (nth 1 ext:*command-args*))))
+        (t (format t "Try harder!~%")
+           (ext:quit 1))))
 
 ;; (foo) 1
 ;; 2
